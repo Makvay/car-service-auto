56ae285eddc9e8050db88b2066b5021bc8d4a7c2
Makvay <bojkov07@list.ru>
2026-04-04 19:42:12 +0800

    feign finished

--- common-service-parent/claim-microservice/src/main/java/ru/car/api/claim/feign/MasterFeignClient.java
+++ common-service-parent/claim-microservice/src/main/java/ru/car/api/claim/feign/MasterFeignClient.java
@@ -1,4 +1,19 @@
 package ru.car.api.claim.feign;
 
-public class MasterFeignClient {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import ru.car.dto.master.MasterDto;
+
+import java.util.List;
+
+@FeignClient(name = "master-service", url = "http://localhost:8083")
+public interface MasterFeignClient {
+
+    @GetMapping("/api/masters/{id}")
+    MasterDto getMasterById(@PathVariable Long id);
+
+    @GetMapping("/api/v1/masters")
+    List<MasterDto> getAllMasters();
+
 }
--- common-service-parent/claim-microservice/src/main/java/ru/car/api/claim/feign/NsiFeignClient.java
+++ common-service-parent/claim-microservice/src/main/java/ru/car/api/claim/feign/NsiFeignClient.java
@@ -1,4 +1,22 @@
 package ru.car.api.claim.feign;
 
-public class NsiFeignClient {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.car.dto.nsi.ServiceDto;
+import ru.car.dto.nsi.VehicleType;
+
+import java.util.List;
+
+@FeignClient(name = "nsi-service", url = "http://localhost:8086")
+public interface NsiFeignClient {
+
+    @GetMapping("/api/nsi/services")
+    List<ServiceDto> getServices();
+
+    @GetMapping("/api/nsi/vehicle-brands")
+    List<?> getVehicleBrands();
+
+    @GetMapping("/api/nsi/part-categories")
+    List<?> getPartCategories();
+
 }
