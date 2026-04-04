970f87e12eabcfefc468f744f65ffa568d0b841b
Makvay <bojkov07@list.ru>
2026-04-04 19:25:20 +0800

    add first feign fix

--- common-service-parent/claim-microservice/src/main/java/ru/car/api/claim/ClaimApplication.java
+++ common-service-parent/claim-microservice/src/main/java/ru/car/api/claim/ClaimApplication.java
@@ -3,6 +3,7 @@ package ru.car.api.claim;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EntityScan(basePackages = {
